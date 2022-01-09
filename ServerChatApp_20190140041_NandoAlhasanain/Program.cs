532e29660aee67e4d4b2592ba064896f306bad72
Nando Alhasanain <Nandoalhasanain@gmail.com>
2022-01-09 17:27:39 +0700

    Add project files.

---
+++ ServerChatApp_20190140041_NandoAlhasanain/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_20190140041;
+
+
+namespace ServerChatApp_20190140041_NandoAlhasanain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready Sir!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
